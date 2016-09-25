2bcc9c7bb22f4ea24c812ee62f6ac87f9fee32f7
james60708 <james60708@gmail.com>
2016-09-26 02:24:01 +0800

    1-1+1-2

--- homework01_B05505004.py
+++ homework01_B05505004.py
@@ -1,4 +1,5 @@
 #1-1
+#coding=utf-8
 import re
 
 samplefile=open("./sample.txt","r")
@@ -21,10 +22,28 @@ for s in sample:
         sampleWordList.append(s)
 
 print(sampleWordList)
-        
-        
+
+samplefile.close()
+
+#1-2
+
+test=input("請輸入任意五個字母以上的單字：")
 
 
+ending=(len(sampleWordList))
+i=0
 
-
-
+if len(test) > 5:
+        while i< ending:
+            for f in sampleWordList:
+                if f==test:
+                    print("您輸入符合sampleWordList裡的字!")
+                    i+=ending
+                    break
+                else:
+                    i+=1
+            else:
+                print("您未輸入相符的字！")
+else:
+    print("您輸入未超過字數底線!")
+    
