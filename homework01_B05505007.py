f7812ff2da12fa98ab4dd20bce58d52b193ea0b2
whu930041 <B05505007@ntu.edu.tw>
2016-09-26 00:39:39 +0800

    PeterWolf==PitaWoof

--- homework01_B05505007.py
+++ homework01_B05505007.py
@@ -1,24 +1,26 @@
-#1-1
+#homework01
 # -*- coding: utf-8 -*-
 #studentID:B05505007
+#1-1
+
 import string                                                  #引入模組
 
 spfile = open("./sample.txt","r")
 sptext = spfile.read()
 spfile.close()
                        
-transform= str.maketrans('0123456789\n',"           ",string.punctuation)
-sptext= sptext.translate(transform)
-splist = sptext.split(" ")
+transform = str.maketrans('0123456789\n',"           ",string.punctuation)
+sptext    = sptext.translate(transform)
+splist    = sptext.split(" ")
 
 sampleWordList = []
 for word in splist: 
     if len(word)>5:
-    	sampleWordList.insert(-1,word)                         #note:.insert(-1,word)==.append(word)
+    	sampleWordList.append(word)                
 
 print("There are words more than five letters.")
 print(sampleWordList)
-print("===============================")
+print("================================================")
 
 #1-2
 
@@ -26,12 +28,12 @@ answer=True
 while answer==True:
     userinput=input("input a word more than 5 letters : ")
     if userinput.lower() in sampleWordList or userinput.title() in sampleWordList:
-        print("it is in the article^.^") 
+        print(userinput.title(),"is in the article^.^") 
     else:
         if len(userinput)<6:
-            print("You idiot lol")
+            print("You idiot,lol.")
         else:
-            print("Sorry it is not found.QQ")
+            print(userinput.title(),"is not found.QQ")
         
  
 
