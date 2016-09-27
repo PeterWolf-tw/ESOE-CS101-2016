f2319c056992bce9d587460f1119569f796c0d1f
james60708 <james60708@gmail.com>
2016-09-27 18:56:19 +0800

    定義一個函數 使輸入錯誤可重新輸入

--- homework01_B05505004.py
+++ homework01_B05505004.py
@@ -21,16 +21,13 @@ samplefile.close()
 
 #1-2
 
-test=input("請輸入任意五個字母以上的單字：")
-
-
-ending=(len(sampleWordList))
-i=0
-
-if len(test) > 5:
-        while i< ending:
+def judge(a):                                                        #定義一個函數始得輸入錯誤可以重新輸入
+    a=input("請輸入一個五個字母以上的英文字:")
+    i=0 
+    if len(a) > 5:                                                   
+        while i< ending:                                             #一個一個嘗試
             for f in sampleWordList:
-                if f==test:
+                if f==a:
                     print("您輸入符合sampleWordList裡的字!")
                     i+=ending
                     break
@@ -38,6 +35,12 @@ if len(test) > 5:
                     i+=1
             else:
                 print("您未輸入相符的字！")
-else:
-    print("您輸入未超過字數底線!")
+    else:
+        print("您輸入未超過字數底線!") 
+        return judge(a)                                               #輸入字母<5可重新輸入
     
+    
+    
+ending=(len(sampleWordList))    
+
+judge("a")
