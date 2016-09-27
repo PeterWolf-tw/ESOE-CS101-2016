295ac6f7d3c7b79b14e72fc6e05d66ca25efbfc8
vat123no <b03303054@ntu.edu.tw>
2016-09-27 17:10:04 +0800

    Update homework01_b03303054.py

--- homework01_b03303054.py
+++ homework01_b03303054.py
@@ -14,5 +14,14 @@ for A in t:
 
 print(sampleWordList)
 
-
+while True:
+    enter=raw_input("輸入5個字母以上的單字")
+    if len(enter)<5:
+        print("單字必須大於5個字母!")
+        
+    elif in sampleWordList:
+        print(enter,"在sampleWordList裡面!")
+        
+    else:
+        print("不在sampleWordList裡面喔!")
 
