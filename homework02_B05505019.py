58c5ec637135e8671ef75509aa047193fcda40dd
vincent861223 <B05505019@ntu.edu.tw>
2016-09-29 14:44:35 +0800

    update answer

--- homework02_B05505019.py
+++ homework02_B05505019.py
@@ -120,16 +120,16 @@ class HW02:
         "xxx" ： 你要填入你的答地方。
         '''
         #作業 5. 課本 Ch3. P3.28
-        self.Ch3P3_28a = ""
-        self.Ch3P3_28b = ""
-        self.Ch3P3_28c = ""
-        self.Ch3P3_28d = ""
+        self.Ch3P3_28a = "765"
+        self.Ch3P3_28b = "439"
+        self.Ch3P3_28c = "-874"
+        self.Ch3P3_28d = "-888"
 
         #作業 6. 課本 Ch3. P3.30
-        self.Ch3P3_30a = ""
-        self.Ch3P3_30b = ""
-        self.Ch3P3_30c = ""
-        self.Ch3P3_30d = "xxx"
+        self.Ch3P3_30a = "766"
+        self.Ch3P3_30b = "440"
+        self.Ch3P3_30c = "-873"
+        self.Ch3P3_30d = "-887"
 
 
 if __name__ == '__main__': #程式進入點，程式由此行開始執行。以下示範助教的批改程式。
