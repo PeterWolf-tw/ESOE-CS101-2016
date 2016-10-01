8a6c4cc1890652482e7b760054fbb38ad1a81bff
benwater12 <b05505033@ntu.edu.tw>
2016-10-01 12:57:55 +0800

    added some answers

--- homework02_b05505033.py
+++ homework02_b05505033.py
@@ -11,4 +11,59 @@ def bin2int(N):
         #print("{0} int {1}.".format(keyed,results)) 測試
     #print("{0} abc {1}.".format(N,results))  測試
     return results
+
+def int2bin(N):
+    tmpLIST = []
+    while N > 0:
+        remainder = int(N % 2)
+        tmpLIST.append(remainder)
+        N = (N - remainder) / 2
+    tmpLIST.append(0)
+    ans = ""
+    for j in tmpLIST[::-1]: #將 tmpLIST 中的數字從尾至頭傳入 j
+        ans = ans + str(j)
+        ans1=int(ans)
+    return ans1
+
+N=1
+while N>0:
+    binder=input("key in=")
+    binder1=int(binder)
+    S=bin2int(binder1)
+    print(S)
+    N=N+1
+
+
     
+    
+    
+
+#作業 2. 課本 Ch2. P2.19
+self.Ch2P2_19a = "10"
+self.Ch2P2_19b = "20"
+self.Ch2P2_19c = "6"
+self.Ch2P2_19d = "8"
+
+#作業 3. 課本 Ch2. P2.20
+self.Ch2P2_20a = "13"
+self.Ch2P2_20b = "7"
+self.Ch2P2_20c = "12"
+self.Ch2P2_20d = "3"
+
+#作業 4. 課本 Ch2. P2.22
+self.Ch2P2_22a = "00010001.11101010.00100010.00001110"
+self.Ch2P2_22b = "00001110.00111000.11101010.00111000"
+self.Ch2P2_22c = "01101110.00001110.00111000.01001110"
+self.Ch2P2_22d = "00011000.00111000.00001101.00001011"    
+
+#作業 5. 課本 Ch3. P3.28
+self.Ch3P3_28a = "xxx"
+self.Ch3P3_28b = "xxx"
+self.Ch3P3_28c = "xxx"
+self.Ch3P3_28d = "xxx"
+
+#作業 6. 課本 Ch3. P3.30
+self.Ch3P3_30a = "xxx"
+self.Ch3P3_30b = "xxx"
+self.Ch3P3_30c = "xxx"
+self.Ch3P3_30d = "xxx"
