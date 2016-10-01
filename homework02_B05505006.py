5e6028a7e3f3746ff52f7a30bef850e161a56bda
ambieneuphoria <B05505006@ntu.edu.tw>
2016-10-01 20:14:03 +0800

    HW#2 #3 completed

--- homework02_B05505006.py
+++ homework02_B05505006.py
@@ -48,22 +48,22 @@ class HW02:
         "xxx" ： 你要填入你的答地方。
         '''
         #作業 2. 課本 Ch2. P2.19
-        self.Ch2P2_19a = "xxx"
-        self.Ch2P2_19b = "xxx"
-        self.Ch2P2_19c = "xxx"
-        self.Ch2P2_19d = "xxx"
+        self.Ch2P2_19a = "10"
+        self.Ch2P2_19b = "17"
+        self.Ch2P2_19c = "6"
+        self.Ch2P2_19d = "8"
 
         #作業 3. 課本 Ch2. P2.20
-        self.Ch2P2_20a = "xxx"
-        self.Ch2P2_20b = "xxx"
-        self.Ch2P2_20c = "xxx"
-        self.Ch2P2_20d = "xxx"
+        self.Ch2P2_20a = "14"
+        self.Ch2P2_20b = "8"
+        self.Ch2P2_20c = "13"
+        self.Ch2P2_20d = "4"
 
         #作業 4. 課本 Ch2. P2.22
-        self.Ch2P2_22a = "xxx"
-        self.Ch2P2_22b = "xxx"
-        self.Ch2P2_22c = "xxx"
-        self.Ch2P2_22d = "xxx"
+        self.Ch2P2_22a = "00010001 11101010 00100010 00001110"
+        self.Ch2P2_22b = "00001110 00111000 11101010 00111000"
+        self.Ch2P2_22c = "01101110 00001110 00111000 01001110"
+        self.Ch2P2_22d = "00011000 00111000 00001101 00001011"
 
 
     def ch3(self):
@@ -78,16 +78,16 @@ class HW02:
         "xxx" ： 你要填入你的答地方。
         '''
         #作業 5. 課本 Ch3. P3.28
-        self.Ch3P3_28a = "xxx"
-        self.Ch3P3_28b = "xxx"
-        self.Ch3P3_28c = "xxx"
-        self.Ch3P3_28d = "xxx"
+        self.Ch3P3_28a = "234"
+        self.Ch3P3_28b = "560"
+        self.Ch3P3_28c = "874"
+        self.Ch3P3_28d = "888"
 
         #作業 6. 課本 Ch3. P3.30
-        self.Ch3P3_30a = "xxx"
-        self.Ch3P3_30b = "xxx"
-        self.Ch3P3_30c = "xxx"
-        self.Ch3P3_30d = "xxx"
+        self.Ch3P3_30a = "234"
+        self.Ch3P3_30b = "560"
+        self.Ch3P3_30c = "875"
+        self.Ch3P3_30d = "889"
 
 
 if __name__ == '__main__': #程式進入點，程式由此行開始執行。以下示範助教的批改程式。
