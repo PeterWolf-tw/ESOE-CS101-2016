8abdc69c0b3c2a9b8ff59df0ff2803083290c832
tzuweiH <b05505013@ntu.edu.tw>
2016-10-02 21:21:27 +0800

    finished hw02ch02

--- homework02_B05505013.py
+++ homework02_B05505013.py
@@ -81,22 +81,22 @@ class HW02:
         "xxx" ： 你要填入你的答地方。
         '''
         #作業 2. 課本 Ch2. P2.19
-        self.Ch2P2_19a = "xxx"
-        self.Ch2P2_19b = "xxx"
-        self.Ch2P2_19c = "xxx"
-        self.Ch2P2_19d = "xxx"
+        self.Ch2P2_19a = "10"
+        self.Ch2P2_19b = "17"
+        self.Ch2P2_19c = "6"
+        self.Ch2P2_19d = "8"
 
         #作業 3. 課本 Ch2. P2.20
-        self.Ch2P2_20a = "xxx"
-        self.Ch2P2_20b = "xxx"
-        self.Ch2P2_20c = "xxx"
-        self.Ch2P2_20d = "xxx"
+        self.Ch2P2_20a = "14"
+        self.Ch2P2_20b = "8"
+        self.Ch2P2_20c = "13"
+        self.Ch2P2_20d = "4"
 
         #作業 4. 課本 Ch2. P2.22
-        self.Ch2P2_22a = "xxx"
-        self.Ch2P2_22b = "xxx"
-        self.Ch2P2_22c = "xxx"
-        self.Ch2P2_22d = "xxx"
+        self.Ch2P2_22a = "00010001 11101010 00100010 00001110"
+        self.Ch2P2_22b = "00001110 00111000 11101010 00111000"
+        self.Ch2P2_22c = "01101110 00001110 00111000 01001110"
+        self.Ch2P2_22d = "00011000 00111000 00001101 00001011"
 
 
     def ch3(self):
