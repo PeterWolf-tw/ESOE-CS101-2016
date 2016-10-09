c4231cdc7ff693ac0cf48bd04d3fa5a8d30cf863
PeterWolf <peterwolf.wang@gmail.com>
2016-10-09 20:34:57 +0800

    程式無法執行，請修正後再上傳。請以規定格式命名你的作業檔案。

--- b05505038HW1.py
+++ b05505038HW1.py
@@ -1,10 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+#前兩行請養成習慣宣告程式種類以及編碼
+
 #b05505038
 #wongchikit
 
 
 sampleWordList = []                 #1-1
-File=open("C:\Users\user\Desktop\sample.txt","r")
-Text=sampleFile.read() 
+#File=open("C:\Users\user\Desktop\sample.txt","r")
+File = open("./sample.txt", "r")
+#用相對路徑的適用性會更廣一些。
+Text=sampleFile.read()
 File.close()
 abc = ["?",":","!",".",",","'"]
 
@@ -12,7 +18,7 @@ abc = ["?",":","!",".",",","'"]
  File=File.replace(p," ")
 newfile=File.split(" ")
 for a in newfile:                    #a=word
-if len(a)>5; 
+if len(a)>5;
 sampleWordList.append(a)
 print(sampleWordList)
 
