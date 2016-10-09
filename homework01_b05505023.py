dc57c12a1c35c4ff25b4ffde7ffcd9f9eac280df
PeterWolf <peterwolf.wang@gmail.com>
2016-10-09 21:50:29 +0800

    內容格式有誤，請修正後再上傳。

--- homework01_b05505023.py
+++ homework01_b05505023.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+#前兩行請養成習慣宣告程式種類以及編碼
+
+#Python 利用 # 做為註解標記。你用 < 是無法讓程式執行的。請參考其它同學的寫法。
 <<<<<<< HEAD
 f=open("./sample.txt","r")
 t=f.read()
-s2=t.replace("."," ") 
+s2=t.replace("."," ")
 s3=s2.replace(","," ")
 s4=s3.replace("-"," ")
 s5=s4.replace("!"," ")
@@ -26,15 +31,15 @@ while z<1000:
     z-=1
     print('not exist')
 
- 
-   
+
+
 
 
 
 =======
 f=open("./sample.txt","r")
 t=f.read()
-s2=t.replace("."," ") 
+s2=t.replace("."," ")
 s3=s2.replace(","," ")
 s4=s3.replace("-"," ")
 s5=s4.replace("!"," ")
@@ -59,8 +64,8 @@ while z<1000:
     z-=1
     print('not exist')
 
- 
-   
+
+
 
 
 
