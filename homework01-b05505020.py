25832a96968cc735fb95c5e9a6779dd6819d4352
Isaiah Yang <m6rmp999@gmail.com>
2016-10-10 22:51:57 +0800

    我把功課做完了

--- homework01-b05505020.py
+++ homework01-b05505020.py
@@ -3,7 +3,7 @@ sampletext = samplefile.read()
 
 
 
-
+finallist = []
 mark = [".","?","(",")",",","!","\"","/",]
 
 for n in mark:
@@ -12,9 +12,28 @@ for n in mark:
 story = sampletext.split(" ")
 
 
-print (story)
+#print (story)
+
+
+
+
+for word in story:
+    if len(word) >= 5:
+        finallist.append(word)
+
+
+
+python = 0
+
+while python == 0:
+    inpu = input("打個字巴")
+    if len(inpu) < 5:
+        print("打個超過五個英文單字的單字巴，別怪我剛剛沒告訴你")
+    elif inpu in finallist:
+        print("Good job, 你打出了finallist的字耶")
+    else:
+        print("你亂打，你可以回家了")
 
     
-
 
 samplefile.close()
