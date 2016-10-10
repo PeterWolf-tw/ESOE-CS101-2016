94c9ef982ed7435ed979f4159821aa1095d69f5a
wyueng <B05505029@ntu.edu.tw>
2016-10-10 23:23:20 +0800

    my god

--- homework02_B05505029.py
+++ homework02_B05505029.py
@@ -56,13 +56,13 @@ def ch2(self):
         "xxx" ： 你要填入你的答地方。
         '''
         #作業 5. 課本 Ch3. P3.28
-        self.Ch3P3_28a = "234"
-        self.Ch3P3_28b = "overflow"
-        self.Ch3P3_28c = "874"
-        self.Ch3P3_28d = "888"
+        self.Ch3P3_28a = "765"
+        self.Ch3P3_28b = "439"
+        self.Ch3P3_28c = "-874"
+        self.Ch3P3_28d = "-888"
 
         #作業 6. 課本 Ch3. P3.30
-        self.Ch3P3_30a = "235"
-        self.Ch3P3_30b = "overflow"
-        self.Ch3P3_30c = "875"
-        self.Ch3P3_30d = "889"
+        self.Ch3P3_30a = "766"
+        self.Ch3P3_30b = "440"
+        self.Ch3P3_30c = "-875"
+        self.Ch3P3_30d = "-889"
