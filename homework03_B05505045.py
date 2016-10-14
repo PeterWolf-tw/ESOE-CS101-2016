63764f3148363d1a1416e11c352030eb449d3942
徐觀 <b05505045@ntu.edu.tw>
2016-10-14 22:25:07 +0800

    version 1

--- homework03_B05505045.py
+++ homework03_B05505045.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+# 3. 請利用以下空白範本設計一支程式。程式可輸入一段字串，並自動計算出字串中包括空白字元出現的機率。
+#    並由高排到低。
+
 def charfreq(inputstr):
     resultlist=[]
     freq=[]
@@ -59,7 +63,22 @@ def condXOR(inputstrx,outputstry):
             outputstr+="1"
 
             return outputSTR
-            
-        
-            
-            
+
+# 5 請完成以下課本習題並將答案以字串型 (str or unicode) 填入。
+print("Ans:")
+Ch3P3_20a = "0100 0000 1110 0110 0000 0000 0000 0000"
+Ch3P3_20b = "1100 0001 0100 1010 0100 0000 0000 0000"
+Ch3P3_20c = "0100 0001 0011 0110 1000 0000 0000 0000"
+Ch3P3_20d = "1011 1110 1100 0000 0000 0000 0000 0000"
+print("========")
+Ch3P3_28a = "234"
+Ch3P3_28b = "overflow"
+Ch3P3_28c = "874"
+Ch3P3_28d = "888"
+print("========")
+Ch3P3_30a = "234"
+Ch3P3_30b = "overflow"
+Ch3P3_30c = "875"
+Ch3P3_30d = "889"
+print("========")
+Ch4P4_3a ="11011101"
