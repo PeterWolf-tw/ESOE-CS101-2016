c4731b09a234b88aa9ea2ee0573c6c979d738284
b05505037 <b05505037@ntu.edu.tw>
2016-10-15 14:08:07 +0800

    reuploaded homework01

--- homework01_b05505037.py
+++ homework01_b05505037.py
@@ -1,7 +1,5 @@
-# B05505037
-# HW1
-
-# HW1-1
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
 
 file = open("./sample.txt","r")
 read_file = file.read()
@@ -13,15 +11,13 @@ read_file = read_file.replace("2,000th","2000th")
 puntuationList = [".",",","!","?","-","\'","\"","\n"]
 for puntuation in puntuationList:
     read_file = read_file.replace(puntuation," ")
-
+    
 wordList = read_file.split(" ")
 for word in wordList:
     if len(word)>5:
         sampleWordList.append(word)
-
+        
 print(sampleWordList)
-
-# HW1-2
 
 inputword = input("Please input a word which includes more than 5 letters.")
 
@@ -36,7 +32,7 @@ else:
         else:
             result = False
         number = number+1
-
+        
 if result == True:
     print(inputword," exists in sampleWordList.")
 else:
