ee6babbb2b58d9f1eb52a81f081f0c6a6cceb3c4
Isaiah Yang <m6rmp999@gmail.com>
2016-10-24 20:15:08 +0800

    土法煉鋼

---
+++ homework03_b05505020.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+
+
+def countingstar(inputstr):
+    a=0
+    b=0
+    c=0
+    d=0
+    e=0
+    f=0
+    g=0
+    h=0
+    i=0
+    j=0
+    k=0
+    l=0
+    m=0
+    n=0
+    o=0
+    p=0
+    q=0
+    r=0
+    s=0
+    t=0
+    u=0
+    v=0
+    w=0
+    x=0
+    y=0
+    z=0
+    for n in inputstr:
+        if n == "a":
+            a=a+1
+        elif n =="b":
+            b=b+1
+            
+    return b
+
+
+ppap=input("打字先")
+
+answer=countingstar(ppap)
+print(answer)
+
+
+
