dbea8d1a8a19940f3a31dcc03dfefa371efe68f9
Isaiah Yang <m6rmp999@gmail.com>
2016-10-24 21:02:01 +0800

    把for迴圈和list做出來，下次要做list的排序

--- homework03_b05505020.py
+++ homework03_b05505020.py
@@ -5,6 +5,10 @@
 
 
 def countingstar(inputstr):
+    inputstr=str(inputstr)
+    total = len(str(inputstr))
+
+    space=[" "]
     a=0
     b=0
     c=0
@@ -31,19 +35,77 @@ def countingstar(inputstr):
     x=0
     y=0
     z=0
-    for n in inputstr:
-        if n == "a":
+    space[0] = 0
+
+    
+    for na in inputstr:
+        if na == "a":
             a=a+1
-        elif n =="b":
-            b=b+1
             
-    return b
+        elif na =="b":
+            b=b+1
+        elif na =="c":
+            c=c+1
+        elif na =="d":
+            d=d+1
+        elif na =="e":
+            e=e+1
+        elif na =="f":
+            f=f+1
+        elif na =="g":
+            g=g+1
+        elif na =="h":
+            h=h+1
+        elif na =="i":
+            i=i+1
+        elif na =="j":
+            j=j+1
+        elif na =="k":
+            k=k+1
+        elif na =="l":
+            l=l+1
+        elif na =="m":
+            m=m+1
+        elif na =="n":
+            n=n+1
+        elif na =="o":
+            o=o+1
+        elif na =="p":
+            p=p+1
+        elif na =="q":
+            q=q+1
+        elif na =="r":
+            r=r+1
+        elif na =="s":
+            s=s+1
+        elif na =="t":
+            t=t+1
+        elif na =="u":
+            u=u+1
+        elif na =="v":
+            v=v+1
+        elif na =="w":
+            w=w+1
+        elif na =="x":
+            x=x+1
+        elif na =="y":
+            y=y+1
+        elif na =="z":
+            z=z+1
+        elif na ==" ":
+            space[0]=space[0]+1
+
+    那該死的list = [(a/total,"a"),(b/total,"b")]
+
+    return 那該死的list
+    #print (a)
+    #print (b)
+    #print (c)
+
 
 
 ppap=input("打字先")
 
 answer=countingstar(ppap)
 print(answer)
-
-
 
