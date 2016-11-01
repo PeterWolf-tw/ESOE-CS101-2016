1a4a38cb89d60f733b0b41a3d610c0c3919a62f3
Peter.w <peterwolf.wang@gmail.com>
2016-11-01 12:41:11 +0800

    homework02 reviewed.

--- homework02_b05505020.py
+++ homework02_b05505020.py
@@ -7,7 +7,7 @@ x=17
 
 
 number = int(x)  #設定 number 這個變數的值為 2
-print("number 的二進位表示法為：{0}".format(bin(number))) 
+print("number 的二進位表示法為：{0}".format(bin(number)))
 
 
 
@@ -19,7 +19,7 @@ def 十進位轉二進位(number):
         number = (number-位數)/2
 
     answer.append("0b")
-    
+
 
     output = ""
     for n in answer[::-1]:
@@ -35,14 +35,14 @@ class HW02:
          #作業 2. 課本 Ch2. P2.19
         self.Ch2P2_19a = "10"
         self.Ch2P2_19b = "17"
-        self.Ch2P2_19c = "7"
-        self.Ch2P2_19d = "9"
+        self.Ch2P2_19c = "7" #"6"
+        self.Ch2P2_19d = "9" #"8"
 
         #作業 3. 課本 Ch2. P2.20
-        self.Ch2P2_20a = "15"
-        self.Ch2P2_20b = "9"
-        self.Ch2P2_20c = "14"
-        self.Ch2P2_20d = "5"
+        self.Ch2P2_20a = "15" #"14"
+        self.Ch2P2_20b = "9"  #"8"
+        self.Ch2P2_20c = "14" #"13"
+        self.Ch2P2_20d = "5"  #"6"
 
         #作業 4. 課本 Ch2. P2.22
         self.Ch2P2_22a = "00010001 11101010 00100010 00001110"
@@ -53,19 +53,19 @@ class HW02:
 
 
     def ch3(self):
-        
-        
+
+
         #作業 5. 課本 Ch3. P3.28
         self.Ch3P3_28a = "234"
-        self.Ch3P3_28b = "overflow"
+        self.Ch3P3_28b = "overflow" #"560"
         self.Ch3P3_28c = "874"
         self.Ch3P3_28d = "888"
 
         #作業 6. 課本 Ch3. P3.30
         self.Ch3P3_30a = "234"
-        self.Ch3P3_30b = "overflow"
+        self.Ch3P3_30b = "overflow" #"560"
         self.Ch3P3_30c = "875"
         self.Ch3P3_30d = "889"
 
 
-        
+
