834f99f61c269dc99be91738c89b028664321a8f
Peter.w <peterwolf.wang@gmail.com>
2016-11-01 13:32:35 +0800

    homework02 reviewed.

--- homework02_B03303054.py
+++ homework02_B03303054.py
@@ -67,7 +67,7 @@ def bin2int(N):
         n= int(n-remainder)/10
         i +=1
         ans +=result
-    print("{0} 的十進位表示為 {1}.".format(N,ans))        
+    print("{0} 的十進位表示為 {1}.".format(N,ans))
     return None
 
 class HW02:
@@ -92,7 +92,7 @@ class HW02:
         self.Ch2P2_20a = "14"
         self.Ch2P2_20b = "8"
         self.Ch2P2_20c = "13"
-        self.Ch2P2_20d = "4"
+        self.Ch2P2_20d = "4" #"6"
 
         #作業 4. 課本 Ch2. P2.22
         self.Ch2P2_22a = "00010001 11101010 00100010 00001110"
@@ -114,13 +114,13 @@ class HW02:
         '''
         #作業 5. 課本 Ch3. P3.28
         self.Ch3P3_28a = "234"
-        self.Ch3P3_28b = "overflow"
+        self.Ch3P3_28b = "overflow" #"560"
         self.Ch3P3_28c = "874"
         self.Ch3P3_28d = "888"
 
         #作業 6. 課本 Ch3. P3.30
         self.Ch3P3_30a = "235"
-        self.Ch3P3_30b = "overflow"
+        self.Ch3P3_30b = "overflow" #"560"
         self.Ch3P3_30c = "875"
         self.Ch3P3_30d = "889"
 
