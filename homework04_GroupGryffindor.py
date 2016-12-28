be7ff6b50f107051b1ad24405749a2a15b50b267
wendy5667 <b05505052@ntu.edu.tw>
2016-12-28 21:33:12 +0800

    QQ

--- homework04_GroupGryffindor.py
+++ homework04_GroupGryffindor.py
@@ -19,9 +19,11 @@ import struct
 
 sound = wave.open("D:/hw/cp/ESOE-CS101-2016/44100.wav")
 nchannels, sampwidth, framerate, nframes, comptype, compname = sound.getparams()
-print(sound.getparams())
+#print(sound.getparams())
 
-showAll = True # Show all data in raw string at once.
+
+raven=[]
+showAll = False # Show all data in raw string at once.
 if showAll == True:
     tapeAll = sound.readframes(nframes)
     
@@ -29,7 +31,16 @@ else:
     for i in range(0, nframes):
         waveData = sound.readframes(1)
         tapeClip = struct.unpack("<h", waveData)
-        print(tapeClip)
+        #print(tapeClip)
+        raven.append(tapeClip[0])
+#print(raven)
+
+nsound = wave.open("D:/hw/cp/GroupGryffindor.wav","wb")
+nsound.setparams((1,2,11025,110250,'NONE','not compressed'))
+
+for i in range(0,441000,4):
+    ntapeClip = struct.pack("<h",raven[i])
+    nsound.writeframes(ntapeClip)
 
 
 # 第二題：請查詢 Python3 的 decode() 文件，利用 Python3 的 decode() 將以下三個字串轉成中文字串並印出。
@@ -54,8 +65,19 @@ print(s0.encode('utf-16'))
 
 # 第四題：請說明 Wifi 和 Bluetooth 之間...
 # (a). 哪一種傳輸方式較為耗電？
+
+#      wifi傳輸速度高 距離遠 所以耗電也比較高
+
+      
 # (b). 哪一種傳輸方式較快速？
+
+#      藍芽傳輸速度720kbps wifi為11mbps所以wifi較快
+       
+
 # (c). 請實際測試：請查出你的手機型號採用的 Bluetooth 規格，再用你的手機拍攝一張照片，
 #      並透過 Bluetooth 傳送該照片到朋友的手機裡。 考量到雙方手機的藍芽設備規格以及照
 #      片的解析度、檔案大小，理論上應該耗時多少時間完成傳送？而實際上又耗了多少時間進行
 #      傳送？ 最後並請列出所有可能影響傳送時間的因素。
+
+#      傳送的照片為2070萬畫素，檔案大小為7.24MB，藍芽4.1傳送至4.0，理論值需57.92秒，
+#      實際需64秒，理論傳輸速度1Mbps，測驗傳輸速度0.953828125Mbps 
