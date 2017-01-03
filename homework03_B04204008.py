882776fc88623ec5c5f93f71c544747a39f43776
franckyychen <b04204008@ntu.edu.tw>
2017-01-03 19:54:06 +0800

    test
    test

--- homework03_B04204008.py
+++ homework03_B04204008.py
@@ -6,7 +6,7 @@
 
 # 作業內容：
 # 1. 請閱讀 Wikipedia 維基百科 IEEE754 條目 (https://zh.wikipedia.org/wiki/IEEE_754)
-
+#
 # 2. 請試玩 http://armorgames.com/play/17826/logical-element
 
 # 3. 請利用以下空白範本設計一支程式。程式可輸入一段字串，並自動計算出字串中包括空白字元出現的機率。
