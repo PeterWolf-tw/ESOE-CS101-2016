9e752de5db88ee909694660b18d5b4f736bb28a8
B05505002 <b05505002@ntu.edu.tw>
2017-01-13 15:42:00 +0800

    homework02

---
+++ homework02-B05505002.py
@@ -0,0 +1,14 @@
+number=input("請輪入一個數字：")
+number2=int(number)
+n=0
+while 10**n<=number2:
+    n=n+1
+n=n-1
+k=0
+while n>=0:
+    t=number2//10**n
+    k=k*2
+    k=k+t
+    number2=number2-t*(10**n)
+    n=n-1
+print(k)
