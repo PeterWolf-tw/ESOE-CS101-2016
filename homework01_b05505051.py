a60b6f849b3d081f485e78b9cc7f0e8c9ca5eafc
lockxuan <b05505051@ntu.edu.tw>
2017-01-13 13:02:54 +0800

    HW1

--- homework01_b05505051.py
+++ homework01_b05505051.py
@@ -1,25 +1,27 @@
--homework1-1
+#homework1-1
 
 sfile=open("sample.txt","r")
 stext=sfile.read()
+sfile.close()
 
 mark=["0","2","-","?",".","\n",",","\"1"]
 swordlist=[]
 
 for element in mark:
-    stext=stext.replace(element,"")
+    stext=stext.replace(element," ")
     
 Stext=stext.split("")
 
 for word in Stext:
     if len(word)>5:
         swordlist.append(word)
+    else:
+        pass
         
 print(swordlist)
-sfile.close()
 
 
--1-2
+#1-2
 while true:
     word=input("輸入一個大於五個字的單字")
     if word in swordlist:
